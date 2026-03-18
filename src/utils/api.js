1dcc2e3fa8697eb57ebc1f451c39cc339ca0eedd
Nipam <nipamparmar72@gmail.com>
2026-03-18 16:10:58 +0530

    fix: update api url for production

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,18 +1,16 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || '/api/v1',
   withCredentials: true,
 })
 
-// attach access token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('accessToken')
   if (token) config.headers.Authorization = `Bearer ${token}`
   return config
 })
 
-// if 401, clear storage and redirect to login
 api.interceptors.response.use(
   (res) => res,
   (err) => {
